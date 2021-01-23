dd8581186e75c6570f1d3857dfdb904958ede62a
conradn <cnsubuga2@gmail.com>
2021-01-24 00:40:27 +0300

    functionality addpatient for single occurances and for files and addpatientlist functionality added

--- Desktop/client.c
+++ Desktop/client.c
@@ -35,18 +35,45 @@ bcopy((char *) server->h_addr , (char *)&serv_addr.sin_addr.s_addr , server->h_l
 serv_addr.sin_port = htons(portno);
 if(connect(sockfd , (struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
 error("Conection failed");
-
-
-
-
-
-
-
-
-
-
-
-
+bzero(buffer,255);
+  
+    
+    char name[255];
+    char gender[255];
+    char cat[255];
+    char choice[255];
+    char apl[255];
+    char txt[] =".txt";
+    char c;
+    int words=0;
+    FILE *f;
+     
+    scanf("%s %s", choice,name);
+    write(sockfd, choice, 255);   						
+    write(sockfd, name, 255);
+    if(strcmp(choice,"addpatient")!=0){
+    printf("command not found!!\n");
+    goto q;}
+    if(strstr(name,txt)){
+     		printf("its a file\n");
+     		f=fopen(name,"r");
+     		for( words=0;(c=getc(f))!=EOF;words++){}
+     		printf("size:%d",words);
+     		write(sockfd, &words, sizeof(int));
+     		rewind(f);
+    		fread(buffer,sizeof(char),512,f);
+       	write(sockfd,buffer,512);
+       	printf("%s",buffer);
+	printf("The file was sent successfully\n");
+	goto q;
+     		}						
+    scanf("%s %s",gender,cat);
+    write(sockfd,gender,255);
+    write(sockfd,cat,255); 
+    scanf("%s",apl);
+    write(sockfd,apl,255);
+    
+q:
 close(sockfd);
 return 0;
 
